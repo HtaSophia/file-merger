22d7d6f237235157980860e67d82a4e3b6a3321c
Aghata Sophia <aghatasophia@gmail.com>
2024-10-24 12:35:05 -0400

    feat: Add the PDF command to the program

--- src/commands/abstract-file.command.ts
+++ src/commands/abstract-file.command.ts
@@ -1,5 +1,5 @@
 import { Command, createCommand } from "commander";
-import { validateFiles } from "../utils/validators/options.validator.js";
+import { validFilesOption } from "../utils/validators/files-option.validator.js";
 import { FileExtension } from "../utils/types/file-extension.js";
 import { FileCommand } from "../utils/types/file-command.js";
 import { CommandAction } from "../utils/types/command-action.js";
@@ -13,7 +13,7 @@ export abstract class AbstractFileCommand implements FileCommand {
         return createCommand(name)
             .description(description)
             .option<string[]>("-f, --files <files>", "List of comma-separated file paths to merge", (value) =>
-                validateFiles(value, type)
+                validFilesOption(value, type)
             )
             .option<string>("-o, --output <output>", "Output file name", (value) => value.trim(), "mergedFile");
     }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import { RootCommand } from "./commands/root.command.js";
+import { PdfCommand } from "./commands/pdf.command.js";
+import { PdfAction } from "./actions/pdf.action.js";
+import { PromptOptions } from "./prompts/helpers/prompt-options.js";
+
+function main() {
+    const program = new RootCommand("file-merger");
+    const promptOptions = new PromptOptions();
+    program.addCommand(new PdfCommand(new PdfAction(promptOptions)).build());
+    program.parse(process.argv);
+}
+
+main();

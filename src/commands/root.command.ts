2b9f2b6ef23fda3ecf00d4d98cbe2c747f449a53
Aghata Sophia <aghatasophia@gmail.com>
2024-10-24 13:13:25 -0400

    refactor: Remove the help option from the root command because it's default

--- src/commands/root.command.ts
+++ src/commands/root.command.ts
@@ -4,11 +4,7 @@ export class RootCommand extends Command {
     public override createCommand(name: string): Command {
         const command = new Command(name);
 
-        command
-            .version("1.0.0")
-            .description("A CLI tool to merge files")
-            .option("-h, --help", "Display help for command");
-
+        command.version("1.0.0").description("CLI tool to merge files");
         command.showSuggestionAfterError();
         command.helpCommand(true);
 

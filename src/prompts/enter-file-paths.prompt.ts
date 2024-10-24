1cee2874d451593324e440daa4b258d0e8e85e25
Aghata Sophia <aghatasophia@gmail.com>
2024-10-24 12:31:04 -0400

    feat: Create Enter File Paths prompt.
    Ask the user to enter the file paths

---
+++ src/prompts/enter-file-paths.prompt.ts
@@ -0,0 +1,24 @@
+import { isCancel, text } from "@clack/prompts";
+import { cancelProcess } from "../utils/helpers/clack/cancel-process.js";
+import { FileExtension } from "../utils/types/file-extension.js";
+import { validFilesOption } from "../utils/validators/files-option.validator.js";
+
+export const enterFilePaths = async (fileExtension: FileExtension): Promise<string[]> => {
+    const filePaths = await text({
+        message: "Enter the file paths (separated by comma):",
+        placeholder: "path/to/file.pdf",
+        validate(value) {
+            if (validFilesOption(value, fileExtension)) {
+                return;
+            }
+
+            return `⚠️ Invalid file paths. Please provide valid file paths.`;
+        },
+    });
+
+    if (isCancel(filePaths)) {
+        cancelProcess();
+    }
+
+    return (filePaths as string).split(",");
+};

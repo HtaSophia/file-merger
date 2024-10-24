3c40a0305e4f142407e43f5726d41d5096d56bbe
Aghata Sophia <aghatasophia@gmail.com>
2024-10-24 00:46:47 -0400

    feat: Create PDF Command to merge PDF files

---
+++ src/commands/pdf.command.ts
@@ -0,0 +1,11 @@
+import { Command } from "commander";
+import { FileExtension } from "../utils/types/file-extension.js";
+import { AbstractFileCommand } from "./abstract-file.command.js";
+
+export class PdfCommand extends AbstractFileCommand {
+    public build(): Command {
+        return this.create("pdf", "merges PDF files", FileExtension.PDF).action(async (options) => {
+            await this.action.handle(options);
+        });
+    }
+}

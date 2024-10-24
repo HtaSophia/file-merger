fa916b3ee76cefe7358f19b552dec4c7efbaa814
Aghata Sophia <aghatasophia@gmail.com>
2024-10-24 12:34:42 -0400

    feat: Create the PDF Action class to handle the PDF command action

---
+++ src/actions/pdf.action.ts
@@ -0,0 +1,14 @@
+import { PromptOptions } from "../prompts/helpers/prompt-options.js";
+import { FileExtension } from "../utils/types/file-extension.js";
+import { CommandOptions } from "../utils/types/command-options.js";
+import { CommandAction } from "../utils/types/command-action.js";
+
+export class PdfAction implements CommandAction {
+    public constructor(private readonly promptOptions: PromptOptions) {}
+
+    public async handle(options: CommandOptions): Promise<void> {
+        const { files, outputFileName } = await this.promptOptions.handleMissingOptions(options, FileExtension.PDF);
+
+        console.log("files", files, "outputFileName", outputFileName);
+    }
+}

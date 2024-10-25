5b629873b6bc159450254445940901c990f8965f
Aghata Sophia <aghatasophia@gmail.com>
2024-10-25 12:19:03 -0400

    refactor: Remove PDF reader and file path validator because it's covered in the File System class

--- src/utils/helpers/readers/pdf-reader.ts
+++
@@ -1,9 +0,0 @@
-import { readdir } from "node:fs/promises";
-import { CURR_DIR } from "../../constants/system-info.const.js";
-import { FileExtension } from "../../types/file-extension.js";
-import { hasExtension } from "../../validators/file-path.validator.js";
-
-export const getFilesFromCurrentDirectory = async (extension: FileExtension): Promise<string[]> => {
-    const files = await readdir(CURR_DIR, "utf-8");
-    return files.filter((file) => hasExtension(file, extension));
-};

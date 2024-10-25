e12395984b5e7642f28d9e15a1af5bf7da7b89c9
Aghata Sophia <aghatasophia@gmail.com>
2024-10-24 23:28:09 -0400

    config: Add Jest and tests config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    rootDir: "./src",
+    displayName: "unit-tests",
+    extensionsToTreatAsEsm: [".ts"],
+    transform: {
+        "^.+\\.(ts)$": [
+            "ts-jest",
+            {
+                useESM: true,
+                tsconfig: "tsconfig.test.json",
+                diagnostics: {
+                    ignoreCodes: ["TS151001"],
+                },
+            },
+        ],
+    },
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    testMatch: ["**/test/**/?(*.)+(spec).[tj]s?(x)"],
+    clearMocks: true,
+};

e6a9fc49e5cc46827ac72637fd7e63799925ee47
Aghata Sophia <aghatasophia@gmail.com>
2024-10-25 15:17:13 -0400

    feat: Update index file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
+import { Command } from "commander";
 import { RootCommand } from "./commands/root.command.js";
+
+import { FileSystem } from "./utils/helpers/file-system/file-system.js";
+import { OptionPrompter } from "./prompts/option-prompter.js";
+import { IOptionPrompter } from "./prompts/option-prompter.interface.js";
+
 import { PdfCommand } from "./commands/pdf.command.js";
 import { PdfAction } from "./actions/pdf.action.js";
-import { PromptOptions } from "./prompts/helpers/prompt-options.js";
 
 function main() {
     const program = new RootCommand("file-merger");
-    const promptOptions = new PromptOptions();
-    program.addCommand(new PdfCommand(new PdfAction(promptOptions)).build());
-    program.parse(process.argv);
+
+    const fileSystem = new FileSystem();
+    const promptOptions = new OptionPrompter(fileSystem);
+
+    program.addCommand(buildPdfCommand(promptOptions));
+    program.parseAsync(process.argv);
 }
+
+const buildPdfCommand = (promptOptions: IOptionPrompter): Command => {
+    const pdfAction = new PdfAction(promptOptions);
+    const pdfCommand = new PdfCommand(pdfAction);
+    return pdfCommand.build();
+};
 
 main();

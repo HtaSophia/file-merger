0d53df364fb752a6a4a5423adfb73db5e4cad5a6
Aghata Sophia <aghatasophia@gmail.com>
2024-10-25 21:12:13 -0400

    feat: Add factory to the index file

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,17 @@ import { RootCommand } from "./commands/root.command.js";
 
 import { FileSystem } from "./core/file-system/file-system.js";
 import { OptionPrompter } from "./prompts/option-prompter.js";
-import { IOptionPrompter } from "./prompts/option-prompter.interface.js";
-
-import { PdfCommand } from "./commands/pdf.command.js";
-import { PdfAction } from "./actions/pdf.action.js";
+import { CommandFactory } from "./commands/command-factory.js";
 
 function main() {
     const program = new RootCommand("file-merger");
 
     const fileSystem = new FileSystem();
     const promptOptions = new OptionPrompter(fileSystem);
+    const commandFactory = new CommandFactory(fileSystem, promptOptions);
 
-    program.addCommand(buildPdfCommand(promptOptions));
+    program.addCommand(commandFactory.createPdfCommand());
     program.parseAsync(process.argv);
 }
-
-const buildPdfCommand = (promptOptions: IOptionPrompter): Command => {
-    const pdfAction = new PdfAction(promptOptions);
-    const pdfCommand = new PdfCommand(pdfAction);
-    return pdfCommand.build();
-};
 
 main();

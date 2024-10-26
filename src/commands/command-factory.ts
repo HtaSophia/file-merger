2f351f4f91ce219c4bd7866edb018fbff40e94f6
Aghata Sophia <aghatasophia@gmail.com>
2024-10-26 18:59:58 -0400

    chore: Rename pdf-merger to pdf.merger

--- src/commands/command-factory.ts
+++ src/commands/command-factory.ts
@@ -4,7 +4,7 @@ import { IOptionPrompter } from "../prompts/option-prompter.interface.js";
 
 import { PdfAction } from "../actions/pdf.action.js";
 import { PdfCommand } from "./pdf.command.js";
-import { PdfMerger } from "../mergers/pdf-merger.js";
+import { PdfMerger } from "../mergers/pdf.merger.js";
 
 export class CommandFactory {
     constructor(private readonly fileSystem: IFileSystem, private readonly promptOptions: IOptionPrompter) {}
